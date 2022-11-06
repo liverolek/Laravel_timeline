4e65d6a6edeb0bfb6ab83faf6c2eaa4bfaaf7583
Aleksander Kotecki <olek@Aleksanders-MacBook-Pro.local>
2022-11-06 15:50:58 +0100

    Fix1

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -1,6 +1,5 @@
 <x-layout>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('../css/timeline.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
    
@@ -12,26 +11,15 @@
         here you find some memories from our community life
     </p>
 
-
-
     <div class="mt-20">
         
-
-      
-       
-
-
         @foreach ($events as $event)
-
-        
-
-       
         <x-timeline date="{{{ $event['date'] }}}
         " title="{{{ $event['title'] }}}" process="{{{$event['process']}}}"
             description="{{{ $event['description'] }}}" logo="{{{ $event['logo'] }}}" id="{{{ $event['id'] }}}"
             status="completed" color="{{$types->find($event['type_id'])->color}}" type="{{$types->find($event['type_id'])->name}} " 
             />
-@endforeach
+        @endforeach
 
     </div>
 
